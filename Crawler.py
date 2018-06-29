ca4d21c10df4b0634d7e514fc4aa8684453f007e
Tristan Anderson <systeatristan@gmail.com>
2018-06-29 01:20:35 -0500

    Basic layout of Crawler

---
+++ Crawler.py
@@ -0,0 +1,31 @@
+class Crawler():
+
+    def __init__(self, seed, maximum, topics, filename):
+        self.seed = seed
+        self.maximum = maximum
+        self.topics = topics
+        self.filename = filename
+
+    def crawl(self):
+        pass
+    
+    def getPage(self):
+        pass
+
+    def checkTopics(self):
+        pass
+
+    def printEdges(self):
+        pass
+
+    def addLinks(self):
+        pass
+
+    def findEdge(self):
+        pass
+
+    def printVisited(self):
+        pass
+
+    def printQueue(self):
+        pass
