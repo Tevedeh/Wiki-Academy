0328ed4752061a17e7033791e31cc687fe2a0a81
Tristan Anderson <systeatristan@gmail.com>
2018-07-14 19:02:58 -0500

    Refactor!

--- Scraper/WikiScrape.py
+++ Scraper/WikiScrape.py
@@ -2,16 +2,13 @@ import datetime
 import time
 import requests
 from bs4 import BeautifulSoup
-
 import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-
-class Crawler():
-
+class WikiScrape():
     baseurl = 'https://en.wikipedia.org'
 
     def __init__(self, seed, maximum, topics, filename):
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
         self.seed = seed
         self.maximum = maximum
         self.topics = topics
@@ -26,36 +23,16 @@ class Crawler():
         self.subStringTime = 0.0
         self.accessInARow = 0
 
-
-    def crawl(self):
-        self.out.write(self.maximum)
-        while len(self.visited) < self.maximum:
-            print(f'RUN! Page is {self.queue[0]}')
-            self.addLinks()
-        self.printEdges()
-        print(self.accessTime)
-        print(self.topicTime)
-        print(self.linkTime)
-        print(self.subStringTime)
-
-    def getPage(self, link):
+    def getPageData(self, link):
         self.startTime = time.time()
         self.accessInARow += 1
         if self.accessInARow >= 25:
             time.sleep(3)
             self.accessInARow = 0
-        #print(self.baseurl+link)
         http = urllib3.PoolManager()
         page = http.request('GET', self.baseurl + link)
-        #soup = BeautifulSoup(page.data, 'html.parser').find_all('a')  
-        soup = BeautifulSoup(page.data, 'html.parser')
-        links = soup.find_all('a')
-        for link in links:
-            mystr = link.get('href')
-            if mystr is None:
-                print("None")
-            elif 'wiki' in mystr:
-                print(link.get('href'))
+        return BeautifulSoup(page.data, 'html.parser')
+    
 
         
 
@@ -79,8 +56,3 @@ class Crawler():
     def printQueue(self):
         for x in self.queue:
             print(x)
-
-
-crawler = Crawler(seed=0, maximum=0, topics=0, filename=0)
-
-crawler.getPage(link='/wiki/Iowa_State_University')
